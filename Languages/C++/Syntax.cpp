df7e0529f5a4fee3ef1bdb2fa2eb63faf153a371
Renziera <stanleyheryanto@gmail.com>
2018-11-03 00:00:23 +0700

    Nambah

---
+++ Languages/C++/Syntax.cpp
@@ -0,0 +1,10 @@
+//contains all standard header files
+#include <bits/stdc++.h>
+
+//so you dont need to specify std:: everytime
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    
+    return 0;
+}
