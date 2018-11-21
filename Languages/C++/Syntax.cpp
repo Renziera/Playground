2c8839b85058a65b8c8c1eaea4c2ff07777c2f8b
Renziera <stanleyheryanto@gmail.com>
2018-11-21 09:21:10 +0700

    Wkwk

--- Languages/C++/Syntax.cpp
+++ Languages/C++/Syntax.cpp
@@ -1,11 +1,9 @@
-//contains all standard header files
+// contains all standard header files
 #include <bits/stdc++.h>
 
-//so you dont need to specify std:: everytime
+// so you dont need to specify std:: everytime
 using namespace std;
 
-int main(int argc, char const *argv[]){
-    
-    
-    return 0;
+int main(int argc, char const* argv[]) {
+  return 0;
 }
